ff4004cefb89351961c152b4052d854315ce828d
Pinke <Pinke@outlook.dk>
2026-03-31 12:00:51 +0200

    Made an IP class to parse an ip address from a string (including test on said class)

---
+++ VLSM/index.js
@@ -0,0 +1,56 @@
+const verbose = {
+  isVerbose: false,
+  log(msg) {
+    if (this.isVerbose)
+      console.log(msg);
+  }
+}
+
+export class ipAddress {
+  constructor(parameters) { this.ipStr = ""; this.octets = [], this.prefix = 0; }
+
+  ipAddressFromString(str) {
+    let elements = str.split("/")
+    verbose.log(elements);
+    if (elements.length === 2) {
+      verbose.log("IP is in CIDR format");
+      // CIDR
+      this.prefix = Number(elements[1]);
+      if (this.prefix < 0 || this.prefix > 32 || isNaN(this.prefix)) {
+        verbose.log("Throwing Error");
+        throw new Error(`CIDR can range from 0 to 32, the ip as a CIDR of ${elements[1]}`);
+      }
+
+      let octetsStrArr = elements[0].split(".");
+      if (octetsStrArr.length !== 4) {
+        verbose.log("Throwing Error");
+        throw new Error(`Missing octets in ip`);
+      }
+
+      for (const i of octetsStrArr) {
+        let j = Number(i);
+        if (isNaN(j)) {
+          verbose.log("Throwing Error");
+          throw new Error(`Ip has inappropiate characters in it`);
+        }
+        if (j > 255 || j < 0) {
+          verbose.log("Throwing Error");
+          throw new Error(`Invalid range on octet ${j}`);
+        }
+        this.octets.push(j)
+      }
+      verbose.log(this.octets);
+      verbose.log("IP Parsed");
+    } else {
+      // Subnet mask
+
+    }
+
+  }
+
+  ipAddressToString() {
+    if (this.octets.length === 4)
+      return `${this.octets[0]}.${this.octets[1]}.${this.octets[2]}.${this.octets[3]}/${this.prefix}`;
+  }
+
+}
---
+++ VLSM/test.js
@@ -0,0 +1,69 @@
+import assert from 'node:assert/strict';
+import { test } from 'node:test';
+
+import { ipAddress } from 'p2vlsm';
+
+// test("name", ()=>{
+// Arrange
+// Act
+// Assert
+// })
+
+
+test("vaild IP", () => {
+
+  // Arrange
+  let ip = new ipAddress();
+
+  // Act & Assert
+  assert.doesNotThrow(() => { ip.ipAddressFromString("192.168.1.1/24") });
+});
+
+test("Character in IP", () => {
+
+  // Arrange
+  let ip = new ipAddress();
+
+  // Act & Assert
+  assert.throws(() => { ip.ipAddressFromString("192.168.abc.1/24") }, "Invalid IP passed");
+});
+
+test("invalid IP", () => {
+
+  // Arrange
+  let ip = new ipAddress();
+
+  // Act & Assert
+  assert.throws(() => { ip.ipAddressFromString("192.168.800.1/24") }, "Invalid IP passed");
+});
+
+test("Invalid prefix", () => {
+
+  // Arrange
+  let ip = new ipAddress();
+
+  // Act & Assert
+  assert.throws(() => { ip.ipAddressFromString("192.168.abc.1/999") }, "Invalid IP passed");
+});
+
+test("Character in Prefix", () => {
+
+  // Arrange
+  let ip = new ipAddress();
+
+  // Act & Assert
+  assert.throws(() => { ip.ipAddressFromString("192.168.abc.1/rt") }, "Invalid IP passed");
+});
+
+test("IP to string", () => {
+
+  // Arrange
+  const str = "192.168.1.1/24";
+  let ip = new ipAddress();
+
+  // act
+  ip.ipAddressFromString(str);
+
+  // Assert
+  assert.equal(ip.ipAddressToString(), str);
+});
