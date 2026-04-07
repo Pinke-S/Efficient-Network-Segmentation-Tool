9234a7fb5c8325c9081a7415d23895c44976c81f
Pinke <Pinke@outlook.dk>
2026-04-07 12:08:51 +0200

    TODO list

--- src-backend/ipaddress.js
+++ src-backend/ipaddress.js
@@ -54,3 +54,10 @@ export class ipAddress {
   }
 
 }
+
+//TODO Santize
+//TODO Get Total host possible
+//TODO Validate Subnet rquirements
+//TODO Correct subnet sizes to closest power of 2.
+//TODO Subnet Object + Subnet Array
+//TODO Alogrithm
