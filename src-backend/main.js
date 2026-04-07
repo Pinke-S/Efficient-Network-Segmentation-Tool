0490354f5f7e732825ca8a94c291015f2c0304d5
Pinke <Pinke@outlook.dk>
2026-04-07 12:27:08 +0200

    Moved todo list

---
+++ src-backend/main.js
@@ -0,0 +1,18 @@
+
+//TODO Santize
+//TODO Get Total host possible
+//TODO Validate Subnet rquirements
+//TODO Correct subnet sizes to closest power of 2.
+//TODO Subnet Object + Subnet Array
+//TODO Alogrithm
+
+/*
+* Subnet object
+* (+) Navn
+* (+) User size
+* (+) Growth size
+* (+) Nearst Block size (Nearest power of 2 (Celling))
+* - CIDR Prefix
+* - IP Address Range
+* - Broadcast and network address
+*/
