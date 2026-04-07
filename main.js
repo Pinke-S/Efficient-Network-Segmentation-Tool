5653d04321f8300b36c5fa08ae75e36970d3e592
Pinke <Pinke@outlook.dk>
2026-04-07 12:32:53 +0200

    Todo Change

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 * - Navn
 * - User size
 * - Growth size
-* - Nearst Block size (Nearest power of 2 (Celling))
+* (+) Nearst Block size (Nearest power of 2 (Celling))
 * (+) CIDR Prefix
 * (+) IP Address Range
 * (+) Broadcast and network address
