56476426dd687674a6c6b4e4de3dabd7d7e7d9b7
Pinke <Pinke@outlook.dk>
2026-04-07 12:25:22 +0200

    Todo

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 //TODO Design Overhaul
 //TODO Collect Data
 //TODO Interactive
+
+
+/*
+* Subnet object
+* - Navn
+* - User size
+* - Growth size
+* - Nearst Block size (Nearest power of 2 (Celling))
+* (+) CIDR Prefix
+* (+) IP Address Range
+* (+) Broadcast and network address
+*/
