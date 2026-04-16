849d690fc01a7e2f9af1eda51934178cc031de04
Laithkar01 <growtopiasamsup@gmail.com>
2026-04-16 13:13:49 +0200

    ændret en fejl hvor jeg glemte +2

--- parsing.js
+++ parsing.js
@@ -1,13 +1,14 @@
- import {getNextPowerOfTwo, getPrefixFromBlockSize} from "./network.js";
-
+import {getNextPowerOfTwo, getPrefixFromBlockSize} from "./network.js";
+     //constructor til subnets,
      export function Subnet(name, host){
-        this.name = name;
-        this.hostRequirement = host;
-        this.nextPowerOfTwo = getNextPowerOfTwo(host);
-        this.prefix = getPrefixFromBlockSize(this.nextPowerOfTwo);
+        this.name = name; // navn hentet fra form
+        this.hostRequirement = host; // host req hentet fra form
+        this.nextPowerOfTwo = getNextPowerOfTwo(host); // næste 2^x, (dette er hvad der rigtigt skal allokeres til)
+        this.prefix = getPrefixFromBlockSize(this.nextPowerOfTwo); // prefix
 
      }
 
+     //funktion til at læse værdier på HTML form (den skal ændres alt efter hvordan front end ser ud)
     export function getFormRows(form) {
         const rows = form.querySelectorAll(".row");
         const objArr = [];
@@ -23,6 +24,7 @@
     }
 
     export function sortSubnets(subnets) {
+         //sortere subnets i descending order
         subnets.sort((a, b) => b.nextPowerOfTwo - a.nextPowerOfTwo);
     }
 
