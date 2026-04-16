0960e5f7b3b2f4a0998330c41f207e653c1f88e8
Laithkar01 <growtopiasamsup@gmail.com>
2026-04-17 01:00:09 +0200

    .

---
+++ src/Subnet/inputValidation.js
@@ -0,0 +1,7 @@
+// Lav Input Validering ift. subnets
+
+//  Har alle subnet felter navn  + host requirement?
+
+// Har alle fælter gyldige karakter?
+
+// Er summen af alle subnets block size <= tilgænlige addresser til ip addresse?
