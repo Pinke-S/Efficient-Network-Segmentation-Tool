ad81ac479b85ef63bc9e12fb485becbe15d8f05a
Pinke <Pinke@outlook.dk>
2026-04-17 11:13:19 +0200

    "merged"... algorithm to main

---
+++ src-backend/ipaddress.js
@@ -0,0 +1,145 @@
+import { verbose } from "./Util.js";
+
+
+/*
+* This class is meant to work as an ip but also a subnet, it has a
+* member called subnets which should contain ipaddress with prefix only until allocated.
+*/
+export class ipAddress {
+
+  constructor() { this.name = "", this.octetsArray = undefined, this.prefix = 0; this.subnets = [] }
+
+  // Meant to easily copy ip addresses and prevent doing it by refrence.
+  copyIp(ipToCopy) {
+    this.name = new String(ipToCopy.String);
+    this.octetsArray = new Uint8Array(ipToCopy.octetsArray);
+    this.prefix = ipToCopy.prefix;
+    this.subnets = new Array(ipToCopy.subnet);
+  }
+
+  // Meant to fill the ip from an Uint8Array and a prefix.
+  ipAddressFromArray(arr, prefix) {
+    if (arr.length !== 4) // If there isn't 4 octets it not an ip
+      throw new Error(`Invalid IP ${arr}`);
+    this.octetsArray = new Uint8Array(arr);
+
+    if (isNaN(prefix) || prefix < 0 || prefix > 32) // Makes sure that the prefix is in the proper range
+      throw new Error(`CIDR can range from 0 to 32, the ip as a CIDR of ${prefix}`);
+    this.prefix = prefix;
+  }
+
+  // Meant to parse a string into an ip
+  ipAddressFromString(str) {
+    let elements = str.split("/") // split the string at the CIDR slash, if it is in CIDR notation
+
+    if (elements.length === 2) { // Checks if it is CIDR notation
+      this.prefix = Number(elements[1]);
+      if (isNaN(this.prefix) || this.prefix < 0 || this.prefix > 32) { // Makes sure that the prefix is in the proper range
+        throw new Error(`CIDR can range from 0 to 32, the ip as a CIDR of ${elements[1]}`);
+      }
+
+
+      let octetsStrArr = elements[0].split("."); // Splits the ip string into octet strings
+      if (octetsStrArr.length !== 4) { // If there isn't 4 octets it not an ip
+        throw new Error(`Missing octets in ip`);
+      }
+
+      let octets = []; // Temporary array to store octets
+      for (const i of octetsStrArr) {
+        let j = Number(i); // Converts an octet to number, if possible
+        if (isNaN(j)) {
+          throw new Error(`Ip has inappropiate characters in it`);
+        }
+
+        if (j > 255 || j < 0) { // Makes sure the octet is in a valid range
+          throw new Error(`Invalid range on octet ${j}`);
+        }
+
+        octets.push(j); // Adds it to the temporary array;
+      }
+
+      this.octetsArray = new Uint8Array(octets); // Converts the array to a Uint8Array to work easier on bits
+    } else {
+      // Subnet mask
+
+    }
+  }
+
+  // Meant to print the ip to string
+  ipAddressToString() {
+    if (!this.octetsArray && this.octetsArray.length !== 4)
+      throw new Error("Array is undefined or Elements are missing in the array");
+
+    // Prints string with template literal string
+    return `${this.octetsArray[0]}.${this.octetsArray[1]}.${this.octetsArray[2]}.${this.octetsArray[3]}/${this.prefix}`;
+  }
+
+  // Set the name of the ip, incase there is a name;
+  setIpName(name) {
+    this.name = new String(name);
+  }
+
+  // gets the total amount of usable hosts
+  getTotalAvailableHosts() {
+    return Math.pow(2, (32 - this.prefix)) - 2; // - 2 to account for the broadcast and the network address
+  }
+
+  // First ip
+  getNetworkAddress() {
+    if (this.octetsArray.length !== 4)  // If there isn't 4 octets it not an ip
+      throw new Error("Array is undefined or Elements are missing in the array");
+
+    let networkAddress = new Uint8Array(this.octetsArray);
+
+    // Sets the remaning bits in the octet with prefix to 0
+    networkAddress[Math.floor(this.prefix / 8)] = networkAddress[Math.floor(this.prefix / 8)] & ((Math.pow(2, (32 - this.prefix) % 8) - 1) ^ 255);
+
+    // Sets the remaning octets to 0 (00000000)
+    for (let index = Math.ceil(this.prefix / 8); index < networkAddress.length; index++)
+      networkAddress[index] = 0;
+
+    return `${networkAddress[0]}.${networkAddress[1]}.${networkAddress[2]}.${networkAddress[3]}/${this.prefix}`;
+
+  }
+
+  // Last ip
+  getBroadcastAddress() {
+    if (this.octetsArray.length !== 4)  // If there isn't 4 octets it not an ip
+      throw new Error("Array is undefined or Elements are missing in the array");
+
+    let broadcastAddress = new Uint8Array(this.octetsArray);
+
+    // Sets the remaning bits in the octet with prefix to 1
+    broadcastAddress[Math.floor(this.prefix / 8)] = broadcastAddress[Math.floor(this.prefix / 8)] | ((Math.pow(2, (32 - this.prefix) % 8) - 1));
+
+    // Sets the remaning octets to 255 (11111111)
+    for (let index = Math.ceil(this.prefix / 8); index < broadcastAddress.length; index++)
+      broadcastAddress[index] = 255;
+
+    return `${broadcastAddress[0]}.${broadcastAddress[1]}.${broadcastAddress[2]}.${broadcastAddress[3]}/${this.prefix}`;
+  }
+
+  // Sort the subnets from lowest prefix first to the highst, by default
+  sortSubnets(cmpfunc) {
+    if (!cmpfunc) {
+      this.subnets.sort((a, b) => { return a.prefix - b.prefix })
+    }
+    else
+      this.subnets.sort(cmpfunc);
+  }
+
+  // Add a singular subnet
+  addSubnet(subnet) {
+    this.subnets.push(new ipAddress());
+    this.subnets[this.subnets.length - 1].copyIp(subnet);
+  }
+
+
+  // Adds an array of subnets
+  addSubnets(subnetarr) {
+    subnetarr.forEach(element => {
+      this.addSubnet(element);
+    });
+  }
+
+}
---
+++ src-backend/main.js
@@ -0,0 +1,20 @@
+
+//TODO Santize @B
+//TODO Get Total host possible @L
+//TODO Validate Subnet rquirements @O
+//TODO Correct subnet sizes to closest power of 2. @L
+//TODO Subnet Object + Subnet Array @l
+//TODO Alogrithm
+
+/*
+* ISP IP Addres string
+* (+) ISP Ip Address as string
+* Subnet object
+* (+) Navn
+* (+) User size
+* (+) Growth size
+* (+) Nearst Block size (Nearest power of 2 (Celling))
+* - CIDR Prefix
+* - IP Address Range
+* - Broadcast and network address
+*/
