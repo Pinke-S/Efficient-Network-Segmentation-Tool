de55e77e1a1c2e747a541b317e305869d16bdc11
BernhardBay <tm38wx@student.aau.dk>
2026-04-21 12:05:15 +0200

    start på validation

--- src/Subnet/inputValidation.js
+++ src/Subnet/inputValidation.js
@@ -5,3 +5,31 @@
 // Har alle fælter gyldige karakter?
 
 // Er summen af alle subnets block size <= tilgænlige addresser til ip addresse?
+function isValidIP(ipAddress) {
+    const parts = ipAddress.split(".");
+
+    // Tjek om der er 4 dele
+    if (parts.length !== 4) {
+        return false;
+    }
+
+    // Tjek hver del af IP addressen
+    for (let i = 0; i < parts.length; i++) {
+        let number = Number(parts[i]);
+
+        if (
+            parts[i] === "" ||          // tomt felt
+            isNaN(number) ||            // ikke et tal
+            number < 0 ||               // mindre end 0
+            number > 255 ||             // større end 255
+            !Number.isInteger(number)   // ikke heltal
+        ) {
+            throw new Error("Invalid IP address");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
